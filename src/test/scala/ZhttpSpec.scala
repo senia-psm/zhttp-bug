9a10804a73674afe59481cde9042ccd01f997f47
Simon Popugaev <senia.psm@gmail.com>
2022-05-02 18:17:27 +0300

    another bug

--- src/test/scala/ZhttpSpec.scala
+++ src/test/scala/ZhttpSpec.scala
@@ -1,5 +1,7 @@
 import zio.*
+import zio.stream.ZStream
 import zio.test.{assertTrue, ZIOSpecDefault}
+import zio.test.TestAspect.timeout
 
 import zhttp.http.*
 import zhttp.service.{ChannelFactory, Client, EventLoopGroup}
@@ -13,7 +15,10 @@ object ZhttpSpec extends ZIOSpecDefault {
       test("Server should be able to start on an unoccupied port") {
 
         val dummyRoute = Http.collect[Request] { case req @ Method.POST -> !! / "echo" =>
-          Response(status = Status.Ok, data = req.data)
+          Response(
+            status = Status.Ok,
+            data = HttpData.fromStream(req.data.toByteBufStream.mapConcat(buf => Chunk.fromByteBuffer(buf.nioBuffer()))),
+          )
         }
 
         val layer: ZLayer[ServerChannelFactory & EventLoopGroup, Nothing, Server.Start] = ZLayer.scoped {
@@ -27,17 +32,19 @@ object ZhttpSpec extends ZIOSpecDefault {
             ChannelFactory.auto,
           )
 
+        val stream = ZStream.repeatZIOChunk(ZIO.sleep(10.millis).as(Chunk[String]("A"))).take(10)
+
         val action = for {
           port           <- ZIO.serviceWith[Server.Start](_.port)
           resp           <- Client.request(
                               s"http://localhost:$port/echo",
                               method = Method.POST,
-                              content = HttpData.fromString("echo string"),
+                              content = HttpData.fromStream(stream),
                             )
           responseString <- resp.bodyAsString
-        } yield assertTrue(port > 0, responseString == "echo string")
+        } yield assertTrue(port > 0, responseString == "AAAAAAAAAA")
 
         action.provideLayer(env)
       },
-    )
+    ) @@ timeout(5.seconds)
 }
